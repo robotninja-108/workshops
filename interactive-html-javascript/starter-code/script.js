3af9894df9bd346c0fdf42fc3ddcff3fe55d7aee
RobotNinja108 <asajfin2@gmail.com>
2026-02-19 18:52:47 -0500

    update

--- interactive-html-javascript/starter-code/script.js
+++ interactive-html-javascript/starter-code/script.js
@@ -12,11 +12,30 @@ const defaultMessage = "Your message appears here.";
 // TODO: Fill in this function to update the preview box.
 const updatePreview = () => {
   // 1) Read the input values
+  const title = titleInput.value;
+  console.log(title)
+  const message = messageInput.value;
+  console.log(message)
+  const color = colorSelect.value;
+  console.log(color)
+  const size = sizeInput.value;
+  console.log(size)
   // 2) Update previewTitle and previewBody
+  previewTitle.textContent = title
+
+
+
   // 3) Update the theme class on messageBox
+  
+  
+  
+  
   // 4) Update the size of messageBox
 };
 
 // TODO: Add event listeners for input + change events.
+titleInput.addEventListener("input", updatePreview);
+
 
 // TODO: Call updatePreview once to set the defaults.
+updatePreview();
